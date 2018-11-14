42d6bcc77837a15d0e3230ce910d7765e8cf176a
jchriest <jchriest@2u.com>
2018-11-14 10:54:01 -0500

    Make Components and get search form to work

---
+++ src/components/result_cards.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ResultCards extends Component{
+
+    render(){
+        return(
+            <div>
+                <h2>{this.props}</h2>
+            </div>
+        )
+    }
+}
