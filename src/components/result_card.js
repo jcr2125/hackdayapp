b43d6ecc2f4dd39402a1e571011bbb90e838ba64
jchriest <jchriest@2u.com>
2018-11-14 16:17:54 -0500

    Done... sort of

--- src/components/result_card.js
+++ src/components/result_card.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 
 export default class ResultCard extends Component{
     render(){
+        // let document = require(`../../public/decisions/${this.props.case_number}.txt`)
+        let document = require(`../../public/decisions/${this.props.case_number}.txt`)
+        
         return(
                 <div className="card">
                     <div className="content">
-                        <div className="header">Case No. {this.props.case_number}</div>
+                        <a href={document} download> <div className="header">Case No. {this.props.case_number}</div> </a>
                     </div>
                 </div>
         )
